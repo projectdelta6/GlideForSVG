efcf21d6b8bf060341765af5f79fdc6cfd47256c
Bradley Duck <projectdelta6@gmail.com>
2020-11-23 16:52:18 +0000

    - Initial commit

---
+++ GlideForSvgModule/src/main/java/com/duck/glideforsvgmodule/SvgGlideModule.kt
@@ -0,0 +1,23 @@
+package com.duck.glideforsvgmodule
+
+import android.content.Context
+import android.graphics.drawable.PictureDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.LibraryGlideModule
+import com.caverock.androidsvg.SVG
+import java.io.InputStream
+
+@GlideModule
+class SvgGlideModule: LibraryGlideModule() {
+    override fun registerComponents(context: Context,
+                                    glide: Glide,
+                                    registry: Registry) {
+        registry.register(
+                SVG::class.java,
+                PictureDrawable::class.java, SvgDrawableTranscoder()
+            )
+            .append(InputStream::class.java, SVG::class.java, SvgDecoder())
+    }
+}
